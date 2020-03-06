d3ea3918001457de2d274a545804e33e91a6e6c0
mark <markkiplagatkipruttoh@gmail.com>
2020-03-06 21:05:44 +0300

    first commit

---
+++ loginsystem/header.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta chaset="ut-8">
+<title>Login System</title>
+<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+   <header>
+     <nav  class="nav-header-main">
+    <a class="header-logo" href="index.php">
+        <img src="img/logo.png" alt="Kip logo">
+       </a>
+       <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">About me</a></li>
+        <li><a href="#">Contact</a></li>
+       </ul>
+     </nav>
+       <div class="header-login">
+         <?php
+         if (isset($_SESSION['userId'])) {
+
+
+            echo '<form action="includes/logout.inc.php" method="post">
+              <button type="submit" name="logout-submit">Logout</button>
+               </form>';
+          }
+          else {
+            echo '<form action="includes/login.inc.php" method="post">
+              <input type="text" name="mailuid" placeholder="E-mail/Username">
+              <input type="password" name="pwd" placeholder="Password">
+              <button type="submit" name="login-submit">Login</button><br>
+            </form>
+            <a href="signup.php" class="header-signup">Signup</a>';
+          }
+          ?>
+
+
+
+
+
+     </div>
+   </header>
+</body>
+
+</html>
